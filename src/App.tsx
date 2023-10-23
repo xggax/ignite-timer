44b836c5b17df26c6b218c6d3b369b66b9db6138
Gabriel Gama <gabrielgamagga@gmail.com>
2023-10-23 16:23:43 -0400

    feat: integracao do styled components com typescript e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+     <GlobalStyle />
      <h1>App</h1>
-    </>
+    </ThemeProvider>
   )
 }
 
