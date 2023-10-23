86c9fb4bd0993843e1cd80dce477183a283590d0
Gabriel Gama <gabrielgamagga@gmail.com>
2023-10-23 17:14:24 -0400

    chore(eslint): mudanca de configuracao do eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-     <GlobalStyle />
-     <h1>App</h1>
+      <GlobalStyle />
+      <h1>App</h1>
     </ThemeProvider>
   )
 }
