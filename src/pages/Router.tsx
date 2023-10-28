68429503879b874135cfeb6a414691180767c29a
Gabriel Gama <gabrielgamagga@gmail.com>
2023-10-28 16:44:57 -0400

    feat[paginas e rotas]: layout de rotas

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './Home'
 import { History } from './History'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
